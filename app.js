3577b25f2842ddad1cfa58374bc30858dc2be0f1
yusufakbs <yusufakbas47@hotmail.com>
2022-01-04 20:39:06 +0300

    Routing

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const express = require('express');
+const pageRoute = require('./routes/pageRoute')
 
 const app = express();
 
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.status(200).send('INDEX SAYFASI');
-});
+// Template Engine
+app.set('view engine', 'ejs');
+
+//Middlewares
+app.use(express.static('public'));
+
+//Routes
+
+app.use('/', pageRoute);
+
+app.use('/about', pageRoute);
 
 app.listen(port, () => {
   console.log(`App started on port ${port}`);

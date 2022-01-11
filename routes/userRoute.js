acfdc78803e43436fe054987c0d636cf5fcf1584
yusufakbs <yusufakbas47@hotmail.com>
2022-01-12 02:35:17 +0300

    Middleware for login, register and dashboard

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const authController  = require('../controllers/authController');
+const authMiddleware = require('../middlewares/authMiddleware');
+
 
 const router = express.Router();
 
 router.route('/signup').post(authController.createUser); // https://localhost:3000/users/signup
 router.route('/login').post(authController.loginUser); // https://localhost:3000/users/signup
 router.route('/logout').get(authController.logoutUser); // https://localhost:3000/users/signup
-router.route('/dashboard').get(authController.getDashboardPage); // https://localhost:3000/users/dashboard
+router.route('/dashboard').get(authMiddleware,authController.getDashboardPage); // https://localhost:3000/users/dashboard
+
 module.exports = router;

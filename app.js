ad15268f1f152f8e0f24031b6985483e90b34b74
yusufakbs <yusufakbas47@hotmail.com>
2022-01-11 03:19:08 +0300

    SessionCreation

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser')
+const session = require('express-session');
+const bodyParser = require('body-parser');
 const pageRoute = require('./routes/pageRoute');
 const courseRoute = require('./routes/courseRoute');
 const categoryRoute = require('./routes/categoryRoute');
@@ -11,27 +12,42 @@ const port = 3000;
 
 // Connect DB
 
-mongoose
-  .connect('mongodb://localhost/smartedu-db')
-  .then(() => {
-    console.log('DB connected Succesfull');
-  });
+mongoose.connect('mongodb://localhost/smartedu-db').then(() => {
+  console.log('DB connected Succesfull');
+});
 
 // Template Engine
 app.set('view engine', 'ejs');
 
+
+// Global Variabla
+
+global.userIN = null;
+
+
 //Middlewares
 app.use(express.static('public'));
-app.use(bodyParser.json()) // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true }))  // for parsing application/x-www-form-urlencoded
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+app.use(
+  session({
+    secret: 'my_keyboard_cat',
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
 //Routes
-
+app.use('*',(req,res,next)=>{
+  userIN = req.session.userID;
+  next();
+})
 app.use('/', pageRoute);
 app.use('/about', pageRoute);
 app.use('/courses', courseRoute);
 app.use('/categories', categoryRoute);
-app.use('/users',userRoute)
+app.use('/users', userRoute);
 
 app.listen(port, () => {
   console.log(`App started on port ${port}`);

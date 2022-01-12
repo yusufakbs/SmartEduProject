89ce7cdc1b4c054fd09af970ecdef15f9cd42d22
yusufakbs <yusufakbas47@hotmail.com>
2022-01-12 23:47:04 +0300

    User Role And Course Creation

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,12 @@
-const User = require('../models/User');
 const bcrypt = require('bcrypt');
+const User = require('../models/User');
+const category = require('../models/Category');
+const Category = require('../models/Category');
+const Course = require('../models/Course');
 exports.createUser = async (req, res) => {
   try {
     const user = await User.create(req.body);
-    res.status(201).json({
-      status: 'success',
-      user,
-    });
+    res.status(201).redirect('/login');
   } catch (error) {
     res.status(400).json({
       status: 'fail',
@@ -46,8 +46,12 @@ exports.logoutUser = (req, res) => {
 
 exports.getDashboardPage = async(req, res) => {
   const user = await User.findOne({_id:req.session.userID});
+  const categories = await Category.find();
+  const courses = await Course.find({user:req.session.userID})
   res.status(200).render('dashboard', {
     page_name: 'dashboard',
-    user
+    user,
+    categories,
+    courses
   });
 };
--- controllers/courseController.js
+++ controllers/courseController.js
@@ -3,11 +3,13 @@ const Category = require('../models/Category');
 
 exports.createCourse = async (req, res) => {
   try {
-    const course = await Course.create(req.body);
-    res.status(201).json({
-      status: 'success',
-      course,
+    const course = await Course.create({
+      name: req.body.name,
+      description: req.body.name,
+      category: req.body.category,
+      user: req.session.userID,
     });
+    res.status(201).redirect('/courses');
   } catch (error) {
     res.status(400).json({
       status: 'fail',
@@ -19,7 +21,7 @@ exports.createCourse = async (req, res) => {
 exports.getAllCourses = async (req, res) => {
   try {
     const categorySlug = req.query.categories;
-    const category = await Category.findOne({slug: categorySlug});
+    const category = await Category.findOne({ slug: categorySlug });
 
     let filter = {};
 
@@ -27,7 +29,7 @@ exports.getAllCourses = async (req, res) => {
       filter = { category: category._id };
     }
 
-    const courses = await Course.find(filter);
+    const courses = await Course.find(filter).sort('-createdAt');
     const categories = await Category.find();
     res.status(200).render('courses', {
       courses,
@@ -44,9 +46,14 @@ exports.getAllCourses = async (req, res) => {
 
 exports.getCourse = async (req, res) => {
   try {
-    const course = await Course.findOne({ slug: req.params.slug });
+
+    const categories = await Category.find();
+    const course = await Course.findOne({ slug: req.params.slug }).populate(
+      'user'
+    );
     res.status(200).render('course', {
       course,
+      categories,
       page_name: 'courses',
     });
   } catch (error) {
